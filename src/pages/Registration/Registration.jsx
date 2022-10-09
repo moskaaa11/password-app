f570700d57508be8e348aa567761d6ca6d1c10fa
moskaaa11 <moskaaa11@gmail.com>
2022-10-09 17:36:57 +0300

    commit

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -11,6 +11,13 @@ const Registration = () => {
       setEnd(true);
   }
 
+  const [newUser, setNewUser] = useState('');
+  const [newMail, setNewMail] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+  const [newAddr, setNewAddr] = useState('');
+  const [newContr, setNewContr] = useState('');
+  const [newPwd, setNewPwd] = useState('');
+
   return (
     <div className='registration'>
       {end ? ( 
@@ -29,19 +36,55 @@ const Registration = () => {
           <div className='registration__form__container'>
             <div className='registration__box'>
               <label className='registration__label' >Username</label>
-              <input className='registration__input' type="text"  placeholder="moskaaa11" required/>
+              <input 
+              className='registration__input' 
+              type="text"  
+              placeholder="moskaaa11"
+              onChange={(e) => setNewUser(e.target.value)} 
+              value={newUser} 
+              required/>
               <label className='registration__label' >Email</label>
-              <input className='registration__input'  type="email"  placeholder="john@gmail.com" required/>
+              <input 
+              className='registration__input'  
+              type="email" 
+              placeholder="john@gmail.com" 
+              onChange={(e) => setNewMail(e.target.value)} 
+              value={newMail} 
+              required/>
               <label className='registration__label' >Phone</label>
-              <input className='registration__input' type="text"  placeholder="+38(091)123-12-12" required/>
+              <input 
+              className='registration__input' 
+              type="text"  
+              placeholder="+38(091)123-12-12" 
+              onChange={(e) => setNewPhone(e.target.value)} 
+              value={newPhone} 
+              required/>
             </div>
             <div className='registration__box'>
               <label className='registration__label' >Password</label>
-              <input className='registration__input' type="password" placeholder='password' required/>
+              <input 
+              className='registration__input' 
+              type="password" 
+              placeholder='password'
+              onChange={(e) => setNewPwd(e.target.value)} 
+              value={newPwd}  
+              required/>
               <label className='registration__label' >Adress</label>
-              <input className='registration__input' type="text"  placeholder="Brooklyn str, 234 Goopy Yd, NY" required/>
+              <input 
+              className='registration__input' 
+              type="text"  
+              placeholder="Brooklyn str, 234 Goopy Yd, NY" 
+              onChange={(e) => setNewAddr(e.target.value)} 
+              value={newAddr} 
+              required/>
               <label className='registration__label' >Country</label>
-              <input className='registration__input' type="text"  placeholder="USA" required/>
+              <input 
+              className='registration__input' 
+              type="text"  
+              placeholder="USA" 
+              onChange={(e) => setNewContr(e.target.value)} 
+              value={newContr} 
+              required/>
             </div>
           </div>
           <button className='registration__button'>Send</button>
